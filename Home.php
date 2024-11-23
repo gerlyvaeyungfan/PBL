9519832d56a48f643d41c6a7b53bdd66dba5b95a
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2024-11-23 15:30:44 +0700

    Upate 1.0

--- Home.php
+++ Home.php
@@ -6,33 +6,91 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Sistem Informasi Tata-Tertib JTI</title>
+    <title>SisiTatib</title>
 </head>
 <body>
 <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">SisiTatib</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+<!--        Icon dan title web di navbar -->
+        <div class="d-flex align-items-center">
+            <img src="image/logoJti.png" class="navbar-brand" width="50" height="50" alt="icon">
+            <a class="navbar-brand mb-0 h1 ml-2 text-primary" href="#">SISTEM INFORMASI TATA TERTIB</a>
+        </div>
+<!--       / Icon dan title web di navbar -->
+
+<!--Navbar-->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    <div class="d-flex align-items-center">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </div>
+
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="#">Bantuan</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link" href="#">Tentang</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
+                    <a class="nav-link" href="#">Profile</a>
                 </li>
             </ul>
         </div>
     </nav>
 </div>
+<div>
+    <img src="image/HomeImage.jpg" class="img-fluid brand-image" alt="Responsive image" width="3500" height="500">
+</div>
+<!--/ navbar -->
+
+<!--Card-->
+<div class="container">
+    <div class="section-title text-center my-4 text-primary ">
+        <h2>PELAPORAN PELANGGARAN </h2>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card text-center shadow-sm p-3 mb-4">
+                <div class="card-body">
+                    <i class="fas fa-headset fa-3x mb-3"></i>
+                    <h5 class="card-title">
+                        <a href="#">Laporan Pelanggaran</a>
+                    </h5>
+                    <p class="card-text">Nullam senectus porttitor in eget. Eget rutrum leo interdum.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card text-center shadow-sm p-3 mb-4">
+                <div class="card-body">
+                    <i class="fas fa-headset fa-3x mb-3"></i>
+                    <h5 class="card-title">
+                        <a href="#">Laporan PPKS</a>
+                    </h5>
+                    <p class="card-text">Cursus semper tellus volutpat aliquet lacus.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card text-center shadow-sm p-3 mb-4">
+                <div class="card-body">
+                    <i class="fas fa-tools fa-3x mb-3"></i>
+                    <h5 class="card-title">
+                        <a href="#">Cek data Laporan</a>
+                    </h5>
+                    <p class="card-text">Ultricies at ipsum nunc, tristique nam lectus.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/Card-->
+
 <!--Script -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
